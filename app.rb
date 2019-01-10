eabab3bf03c4fa327014ae3690bb9e771a4a6fd9
Jacques <jacques.laffy@gmail.com>
2019-01-10 11:39:55 +0000

    User Story 1 - Sign Up

--- app.rb
+++ app.rb
@@ -2,9 +2,25 @@ require 'sinatra'
 require 'sinatra/activerecord'
 
 class App < Sinatra::Base
+  enable :sessions
 
   get '/' do
-    "Hello world!"
+    erb(:index, :layout => :layout)
   end
 
+  get '/signup' do
+    erb(:signup, :layout => :layout)
+  end
+
+  post '/signup' do
+    session[:email] = params[:email]
+    session[:password] = params[:password]
+    redirect '/profile'
+  end
+
+  get '/profile' do
+    @email = session[:email]
+    @password = session[:password]
+    erb(:profile, :layout => :layout)
+  end
 end
