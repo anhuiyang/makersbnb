ec0585a2682efd7266f81243453350106e2a999e
stonefarmer9 <g.p.levaillant@gmail.com>
2019-01-10 11:56:53 +0000

    Merge complete - User stories 1 & 2 complete to be refactored

--- spec/features/user_logout_spec.rb
+++ spec/features/user_logout_spec.rb
@@ -3,7 +3,7 @@ require_relative 'web_helper'
 feature 'logging out' do
   scenario 'User can log out' do
     log_in
-    click_link 'log out'
-    expect(page).to have_link 'log in'
+    click_on 'log out'
+    expect(page).to have_button 'log in'
   end
 end
--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -1,7 +1,7 @@
 def log_in
   visit '/'
-  click_link 'log in'
+  click_on 'log in'
   fill_in('email', :with => 'user@email.com')
   fill_in('password', :with => 'secret123')
-  click_button 'log in'
+  click_on'log in'
 end
